c4eb63d63522b4293aae2a98a4d4643e61e5fb4c
Nav-Krish <navanethakrishnan11@gmail.com>
2023-11-26 20:56:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/Navbar";
-import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import ForgotPassword from "./components/ForgotPassword";
